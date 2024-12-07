be9ccd3b400ca620e6863d03f3adf26beb71aff4
Hardik Sharma <hardiksharmah73@gmail.com>
2024-12-07 12:59:01 +0530

    simple decorator with wrapas

--- decorators/decorator.py
+++ decorators/decorator.py
@@ -1,9 +1,12 @@
-# Simple Decorator
+# Simple Decorator with wraps
+from functools import wraps
+
 def decorator( tgtFunction ):
 	# tgtFunction is the function to be wrapped.
+	@wraps(tgtFunction) # NEW LINE ADDED
 	def wrapper(*args, **kwargs):
 		# Do pre-defined pre-processing
-		print(f"Calling {tgtFunction.__name__} with {args}, {kwargs}")
+		print(f"Calling {tgtFunction.__qualname__} with {args}, {kwargs}")
 		return tgtFunction(*args, **kwargs)
 	
 	return wrapper
